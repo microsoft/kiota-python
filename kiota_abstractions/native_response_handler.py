4f7b0d207a5143ab04e37204b5270a9fa33cd7bf
samwelkanda <samwelkanda@gmail.com>
2023-01-18 19:44:40 +0300

    Update method documentation

--- kiota_abstractions/native_response_handler.py
+++ kiota_abstractions/native_response_handler.py
@@ -16,4 +16,12 @@ class NativeResponseHandler(ResponseHandler):
         response: NativeResponseType,
         error_map: Optional[Dict[str, Optional[ParsableFactory]]] = None
     ) -> NativeResponseType:
+        """Callback method that is invoked when a response is received.
+        Args:
+            response (NativeResponseType): The type of the native response object.
+            error_map (Optional[Dict[str, Optional[ParsableFactory]]]): the error dict to use
+            in case of a failed request.
+        Returns:
+            Any: The native response object.
+        """
         return response
--- kiota_abstractions/response_handler.py
+++ kiota_abstractions/response_handler.py
@@ -18,7 +18,7 @@ class ResponseHandler(ABC):
         """Callback method that is invoked when a response is received.
         Args:
             response (NativeResponseType): The type of the native response object.
-            error_map (Dict[str, Optional[ParsableFactory]]): the error dict to use
+            error_map (Optional[Dict[str, Optional[ParsableFactory]]]): the error dict to use
             in case of a failed request.
         Returns:
             Any: The deserialized response.
