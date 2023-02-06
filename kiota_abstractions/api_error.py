1e782bbc62311d80fd3e53010af831c73db9109d
samwelkanda <samwelkanda@gmail.com>
2023-02-06 08:21:39 +0300

    Add response status code to APIError

--- kiota_abstractions/api_error.py
+++ kiota_abstractions/api_error.py
@@ -1,4 +1,16 @@
 class APIError(Exception):
+    """The base class for all API errors."""
 
-    def __init__(self, message: str) -> None:
-        super().__init__(message)
+    def __init__(self, status_code: int, message: str) -> None:
+        self._response_status_code = status_code
+        self._message = message
+        super().__init__(self._message)
+
+    @property
+    def response_status_code(self) -> int:
+        """The HTTP status code of the response that caused the error."""
+        return self._response_status_code
+
+    @response_status_code.setter
+    def response_status_code(self, value: int) -> None:
+        self._response_status_code = value
