c57f5aad6c15ba66ca97dbcdd1c7defbb6780703
samwelkanda <samwelkanda@gmail.com>
2023-05-20 02:57:33 +0300

    Update error class to work with pydantic models

--- kiota_abstractions/api_error.py
+++ kiota_abstractions/api_error.py
@@ -1,29 +1,12 @@
-from typing import Dict
+from typing import Dict, Optional
+from pydantic.dataclasses import dataclass
 
-
+@dataclass
 class APIError(Exception):
     """The base class for all API errors."""
 
-    def __init__(self, message: str, headers: Dict[str, str], status_code: int) -> None:
-        self._response_headers = headers
-        self._response_status_code = status_code
-        self._message = message
-        super().__init__(self._message)
-
-    @property
-    def response_headers(self) -> Dict[str, str]:
-        """The HTTP headers of the response that caused the error."""
-        return self._response_headers
-
-    @response_headers.setter
-    def response_headers(self, value: Dict[str, str]) -> None:
-        self._response_headers = value
-
-    @property
-    def response_status_code(self) -> int:
-        """The HTTP status code of the response that caused the error."""
-        return self._response_status_code
-
-    @response_status_code.setter
-    def response_status_code(self, value: int) -> None:
-        self._response_status_code = value
+    error_message: Optional[str] = None
+    # The HTTP headers of the response that caused the error.
+    response_headers: Optional[Dict[str, str]] = None
+    # The HTTP status code of the response that caused the error.
+    response_status_code: Optional[int] = None
