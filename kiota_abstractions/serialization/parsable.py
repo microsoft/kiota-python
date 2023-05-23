64887550c62c1dcfcdde5c8ab26b2b265b2bba1f
samwelkanda <samwelkanda@gmail.com>
2023-05-23 14:40:40 +0300

    Update parsable to become dataclass and add convenience methods

--- kiota_abstractions/serialization/parsable.py
+++ kiota_abstractions/serialization/parsable.py
@@ -1,5 +1,7 @@
-from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING, Any, Callable, Dict, TypeVar
+from dataclasses import asdict, dataclass
+from json import dumps
+from typing import TYPE_CHECKING, Any, Callable, Dict, Protocol, TypeVar
+
 
 T = TypeVar("T")
 
@@ -7,13 +9,18 @@ if TYPE_CHECKING:
     from .parse_node import ParseNode
     from .serialization_writer import SerializationWriter
 
-
-class Parsable(ABC):
+@dataclass
+class Parsable(Protocol):
     """
     Defines a serializable model object.
     """
+    
+    def to_dict(self):
+        return asdict(self)
 
-    @abstractmethod
+    def to_json(self):
+        return dumps(asdict(self), default=str)
+
     def get_field_deserializers(self) -> Dict[str, Callable[['ParseNode'], None]]:
         """Gets the deserialization information for this object.
 
@@ -21,13 +28,13 @@ class Parsable(ABC):
             Dict[str, Callable[[ParseNode], None]]: The deserialization information for this
             object where each entry is a property key with its deserialization callback.
         """
-        pass
+        ...
 
-    @abstractmethod
     def serialize(self, writer: 'SerializationWriter') -> None:
         """Writes the objects properties to the current writer.
 
         Args:
             writer (SerializationWriter): The writer to write to.
         """
-        pass
+        ...
+
