8e702c3b0c1238c796a921d9816ba0e242eccbbe
Martin Musale <martinmusale@microsoft.com>
2023-06-27 14:02:40 +0100

    Make public exports available from module using __all__
    Signed-off-by: Martin Musale <martinmusale@microsoft.com>

--- kiota_abstractions/authentication/__init__.py
+++ kiota_abstractions/authentication/__init__.py
@@ -2,4 +2,17 @@ from .access_token_provider import AccessTokenProvider
 from .allowed_hosts_validator import AllowedHostsValidator
 from .anonymous_authentication_provider import AnonymousAuthenticationProvider
 from .authentication_provider import AuthenticationProvider
-from .base_bearer_token_authentication_provider import BaseBearerTokenAuthenticationProvider
+from .base_bearer_token_authentication_provider import (
+    BaseBearerTokenAuthenticationProvider,
+)
+from .api_key_authentication_provider import ApiKeyAuthenticationProvider, KeyLocation
+
+__all__ = [
+    "AccessTokenProvider",
+    "AllowedHostsValidator",
+    "AnonymousAuthenticationProvider",
+    "AuthenticationProvider",
+    "BaseBearerTokenAuthenticationProvider",
+    "ApiKeyAuthenticationProvider",
+    "KeyLocation",
+]
