abfd80c47d1b28319bbe024c0935aea19273f3dc
Martin Musale <martinmusale@microsoft.com>
2023-06-29 16:52:14 +0100

    Use staticmethod on get_key()
    The usage of RequestOptions in [kiota http](https://github.com/microsoft/kiota-http-python/blob/main/kiota_http/kiota_client_factory.py\#L101-L119) is of the signature of a staticmethod. This update ensures that the abstractmethod is exposed as an abstract and static method
    
    Signed-off-by: Martin Musale <martinmusale@microsoft.com>

--- kiota_abstractions/request_option.py
+++ kiota_abstractions/request_option.py
@@ -5,11 +5,11 @@ class RequestOption(ABC):
     """Represents a request option
     """
 
+    @staticmethod
     @abstractmethod
-    def get_key(self) -> str:
+    def get_key() -> str:
         """Gets the option key for when adding it to a request. Must be unique
 
         Returns:
             str: The option key
         """
-        pass
