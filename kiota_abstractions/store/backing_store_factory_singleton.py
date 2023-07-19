00085319bcf9d41c141ee8d37043133f8226235e
samwelkanda <samwelkanda@gmail.com>
2023-07-19 18:24:51 +0300

    Code formatting

--- kiota_abstractions/store/backing_store_factory_singleton.py
+++ kiota_abstractions/store/backing_store_factory_singleton.py
@@ -1,9 +1,13 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation.  All Rights Reserved.
+# Licensed under the MIT License.
+# See License in the project root for license information.
+# ------------------------------------------------------------------------------
+
 from typing_extensions import Self
 
 from kiota_abstractions.store.backing_store_factory import BackingStoreFactory
-from kiota_abstractions.store.in_memory_backing_store_factory import (
-    InMemoryBackingStoreFactory,
-)
+from kiota_abstractions.store.in_memory_backing_store_factory import InMemoryBackingStoreFactory
 
 
 class BackingStoreFactorySingleton:
--- kiota_abstractions/store/backing_store_parse_node_factory.py
+++ kiota_abstractions/store/backing_store_parse_node_factory.py
@@ -1,3 +1,9 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation.  All Rights Reserved.
+# Licensed under the MIT License.
+# See License in the project root for license information.
+# ------------------------------------------------------------------------------
+
 from typing import Callable
 
 from ..serialization import Parsable, ParseNodeFactory, ParseNodeProxyFactory
