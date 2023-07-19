18d5d4299d7e415111d325074f3d96ad7436b741
samwelkanda <samwelkanda@gmail.com>
2023-07-19 18:23:48 +0300

    Convert backed model to a dataclass

--- kiota_abstractions/serialization/serialization_writer_proxy_factory.py
+++ kiota_abstractions/serialization/serialization_writer_proxy_factory.py
@@ -1,3 +1,9 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation.  All Rights Reserved.
+# Licensed under the MIT License.
+# See License in the project root for license information.
+# ------------------------------------------------------------------------------
+
 from typing import Callable, Optional
 
 from .parsable import Parsable
--- kiota_abstractions/store/backed_model.py
+++ kiota_abstractions/store/backed_model.py
@@ -1,17 +1,33 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation.  All Rights Reserved.
+# Licensed under the MIT License.
+# See License in the project root for license information.
+# ------------------------------------------------------------------------------
+
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Optional
 
 from .backing_store import BackingStore
 
 
-class BackedModel(ABC):
+@dataclass
+class BackedModel:
     """Defines the contracts for a model that is backed by a store.
     """
+    # Stores model information.
+    backing_store: BackingStore
 
-    @abstractmethod
-    def get_backing_store(self) -> BackingStore:
-        """Gets the store that is backing the model
+    def __setattr__(self, prop, val):
+        if not prop == "backing_store":
+            self.__backing_store.set(prop, val)
+            super().__setattr__(prop, self.__backing_store.get(prop))
+        super().__setattr__(prop, val)
 
-        Returns:
-            BackingStore: The store backing the model
-        """
-        pass
+    @property
+    def __backing_store(self):
+        return self.backing_store
+
+    @__backing_store.setter
+    def __backing_store(self, value):
+        self.backing_store = value
