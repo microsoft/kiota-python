53d06950f897d61e969c2daec0175b12fc8bf981
samwelkanda <samwelkanda@gmail.com>
2023-07-19 18:25:36 +0300

    Add tests forinmemory backing store

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
-from typing import Callable, List, Any, Tuple
+from __future__ import annotations
+from dataclasses import dataclass, field
+from typing import Callable, List, Any, Tuple, Dict, Optional
 import pytest
 
 from kiota_abstractions.authentication.access_token_provider import AccessTokenProvider
@@ -6,11 +8,8 @@ from kiota_abstractions.authentication.allowed_hosts_validator import (
     AllowedHostsValidator,
 )
 from kiota_abstractions.request_information import RequestInformation
-from kiota_abstractions.store import (
-    BackingStore,
-    InMemoryBackingStore,
-    BackingStoreFactory,
-)
+from kiota_abstractions.serialization import AdditionalDataHolder, Parsable, ParseNode, SerializationWriter
+from kiota_abstractions.store import BackedModel, BackingStore, BackingStoreFactorySingleton
 
 
 class MockAccessTokenProvider(AccessTokenProvider):
@@ -23,6 +22,72 @@ class MockAccessTokenProvider(AccessTokenProvider):
     def get_allowed_hosts_validator(self) -> AllowedHostsValidator:
         return AllowedHostsValidator(["example.com"])
 
+@dataclass
+class MockEntity(Parsable, AdditionalDataHolder, BackedModel):
+    # Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.
+    additional_data: Dict[str, Any] = field(default_factory=dict)
+    # Stores model information.
+    backing_store : BackingStore = field(default_factory=BackingStoreFactorySingleton(backing_store_factory=None).backing_store_factory.create_backing_store)
+    # The id property
+    id: Optional[str] = None
+    # The OdataType property
+    odata_type: str = "#microsoft.graph.mockEntity"
+    # The telephone numbers for the user. NOTE: Although this is a string collection, only one number can be set for this property. Read-only for users synced from on-premises directory. Returned by default. Supports $filter (eq, not, ge, le, startsWith).
+    business_phones: Optional[List[str]] = None
+    # The user or contact that is this user&apos;s manager. Read-only. (HTTP Methods: GET, PUT, DELETE.). Supports $expand.
+    manager: Optional[MockEntity] = None
+    # The user or contact that is this user& works with.
+    colleagues: Optional[List[MockEntity]] = None
+    
+    @staticmethod
+    def create_from_discriminator_value(parse_node: Optional[ParseNode] = None):
+        """
+        Creates a new instance of the appropriate class based on discriminator value
+        Args:
+            parse_node: The parse node to use to read the discriminator value and create the object
+        Returns: AccessAction
+        """
+        if not parse_node:
+            raise TypeError("parse_node cannot be null.")
+        return MockEntity()
+    
+    def get_field_deserializers(self,) -> Dict[str, Callable[[ParseNode], None]]:
+        """
+        The deserialization information for the current model
+        Returns: Dict[str, Callable[[ParseNode], None]]
+        """
+        fields: Dict[str, Callable[[Any], None]] = {
+            "@odata.type": lambda n : setattr(self, 'odata_type', n.get_str_value()),
+            "id": lambda n : setattr(self, 'id', n.get_str_value()),
+            "businessPhones": lambda n : setattr(self, 'business_phones', n.get_collection_of_primitive_values(str)),
+            "manager": lambda n : setattr(self, 'manager', n.get_object_value(MockEntity)),
+            "colleagues": lambda n : setattr(self, 'colleagues', n.get_collection_of_object_values(MockEntity)),
+        }
+        return fields
+    
+    def serialize(self,writer: SerializationWriter) -> None:
+        """
+        Serializes information the current object
+        Args:
+            writer: Serialization writer to use to serialize this model
+        """
+        if not writer:
+            raise TypeError("writer cannot be null.")
+        writer.write_str_value("@odata.type", self.odata_type)
+        writer.write_additional_data_value(self.additional_data)
+        writer.write_str_value("id", self.id)
+        writer.write_collection_of_primitive_values("businessPhones", self.business_phones)
+        writer.write_object_value("manager", self.manager)
+        writer.write_collection_of_object_values("colleagues", self.colleagues)
+    
+@pytest.fixture
+def mock_user():
+    user = MockEntity()
+    user.id = "84b3f7bf-6afb-46b2-9c6d-660c9b8c8ea0"
+    user.additional_data = {"extensionData": None}
+    user.business_phones = ["+1 732 555 0102"]
+    user.backing_store.is_initialization_completed = True
+    return user
 
 @pytest.fixture
 def mock_request_information():
---
+++ tests/store/test_in_memory_backing_store.py
@@ -0,0 +1,195 @@
+import pytest
+
+from kiota_abstractions.store import InMemoryBackingStore
+
+from tests.conftest import MockEntity
+
+def test_prevents_adding_empty_keys():
+    backing_store = InMemoryBackingStore()
+    with pytest.raises(ValueError):
+        backing_store.set("", "Samwel")
+        
+def test_sets_and_gets_value_from_backing_store():
+    backing_store = InMemoryBackingStore()
+    assert not backing_store.enumerate_()
+    backing_store.set("name", "Samwel")
+    assert backing_store.enumerate_() == [("name", "Samwel")]
+    
+def test_prevents_duplicates_in_store():
+    backing_store = InMemoryBackingStore()
+    assert not backing_store.enumerate_()
+    backing_store.set("name", "Samwel")
+    backing_store.set("name", "Samwel 2")
+    assert backing_store.enumerate_() == [("name", "Samwel 2")]
+    
+def test_enumerates_values_changed_to_none_in_store():
+    backing_store = InMemoryBackingStore()
+    assert not backing_store.enumerate_()
+    backing_store.set("name", "Samwel")
+    backing_store.set("email", "samwel@example.com")
+    backing_store.set("phone", None) # remove phone
+    assert backing_store.enumerate_keys_for_values_changed_to_null()
+    assert backing_store.enumerate_keys_for_values_changed_to_null() == ["phone"]
+    assert len(backing_store.enumerate_()) == 3
+    
+def test_backing_store_embedded_in_model(mock_user):
+    mock_user.business_phones = ["+1 234 567 891"]
+    mock_user.backing_store.return_only_changed_values = True
+    changed_values = dict(mock_user.backing_store.enumerate_())
+    assert len(changed_values) == 1
+    assert "business_phones" in changed_values
+    
+
+    
+def tests_backing_store_embedded_in_model_with_additonal_data_values(mock_user):
+    mock_user.business_phones = ["+1 234 567 891"]
+    mock_user.additional_data = {"anotherExtension": None}
+    mock_user.backing_store.return_only_changed_values = True
+    changed_values = dict(mock_user.backing_store.enumerate_())
+    assert len(changed_values) == 2
+    assert "business_phones" in changed_values
+    assert "additional_data" in changed_values
+    
+def test_backing_store_embedded_in_model_with_collection_property_replaced_with_new_collection(mock_user):
+    mock_user.business_phones = ["+1 234 567 891"]
+    mock_user.backing_store.return_only_changed_values = True
+    changed_values = mock_user.backing_store.enumerate_()
+    assert len(changed_values) == 1
+    assert  changed_values[0][0] == "business_phones"
+    business_phones = mock_user.backing_store.get("business_phones")
+    assert business_phones == ["+1 234 567 891"]
+    
+def test_backing_store_embedded_in_model_with_collection_property_replaced_with_none(mock_user):
+    mock_user.business_phones = None
+    mock_user.backing_store.return_only_changed_values = True
+    changed_values = mock_user.backing_store.enumerate_()
+    assert len(changed_values) == 1
+    assert  changed_values[0][0] == "business_phones"
+    assert changed_values[0][1] == None
+    values_changed_to_null = mock_user.backing_store.enumerate_keys_for_values_changed_to_null()
+    assert values_changed_to_null == ["business_phones"]
+    
+def test_backing_store_embedded_in_model_with_collection_property_modified_by_append(mock_user):
+    mock_user.business_phones.append("+1 234 567 891")
+    mock_user.backing_store.return_only_changed_values = True
+    changed_values = mock_user.backing_store.enumerate_()
+    assert len(changed_values) == 1
+    assert  changed_values[0][0] == "business_phones"
+    assert len(mock_user.backing_store.get("business_phones")) == 2 # 2 items in collection as the property was modified by add
+    
+def test_backing_store_embedded_in_model_by_setting_nested_backed_model(mock_user):
+    mock_user.manager = MockEntity(id="2f8c9b8c-8ea0-46b2-9c6d-660c9b7bf6af")
+    mock_user.backing_store.return_only_changed_values = True
+    changed_values = mock_user.backing_store.enumerate_()
+    assert len(changed_values) == 1
+    assert  changed_values[0][0] == "manager"
+    manager = changed_values[0][1]
+    assert manager.id == "2f8c9b8c-8ea0-46b2-9c6d-660c9b7bf6af"
+    
+def test_backing_store_embedded_in_model_by_updating_nested_backed_model():
+    mock_user = MockEntity(
+        id="84c747c1-d2c0-410d-ba50-fc23e0b4abbe",
+        manager=MockEntity(id="2f8c9b8c-8ea0-46b2-9c6d-660c9b7bf6af")
+    )
+    mock_user.backing_store.is_initialization_completed = mock_user.manager.backing_store.is_initialization_completed = True
+    mock_user.manager.business_phones = ["+1 234 567 891"]
+    mock_user.backing_store.return_only_changed_values = True
+    changed_values = mock_user.backing_store.enumerate_()
+    assert len(changed_values) == 1
+    assert  changed_values[0][0] == "manager" # Backingstore should detect manager property changed
+    
+def test_backing_store_embedded_in_model_by_updating_nested_backed_model_returns_changed_nested_properties():
+    mock_user = MockEntity(
+        id="84c747c1-d2c0-410d-ba50-fc23e0b4abbe",
+        manager=MockEntity(id="2f8c9b8c-8ea0-46b2-9c6d-660c9b7bf6af")
+    )
+    mock_user.backing_store.is_initialization_completed = mock_user.manager.backing_store.is_initialization_completed = True
+    mock_user.manager.business_phones = ["+1 234 567 891"]
+    mock_user.backing_store.return_only_changed_values = True
+    mock_user.manager.backing_store.return_only_changed_values = True
+    changed_values = mock_user.backing_store.enumerate_()
+    assert len(changed_values) == 1
+    assert  changed_values[0][0] == "manager" # Backingstore should detect manager property changed
+    nested_changed_values = mock_user.manager.backing_store.enumerate_()
+    assert len(nested_changed_values) == 1
+    assert nested_changed_values[0][0] == "business_phones"
+    
+def test_backing_store_embedded_in_model_by_updating_nested_backed_model_collection_property():
+    mock_user = MockEntity(
+        id="84c747c1-d2c0-410d-ba50-fc23e0b4abbe",
+        colleagues=[MockEntity(id="2f8c9b8c-8ea0-46b2-9c6d-660c9b7bf6af")]
+    )
+    mock_user.backing_store.is_initialization_completed = mock_user.colleagues[0].backing_store.is_initialization_completed = True
+    mock_user.colleagues[0].business_phones = ["+1 234 567 891"]
+    mock_user.backing_store.return_only_changed_values = True
+    changed_values = mock_user.backing_store.enumerate_()
+    assert len(changed_values) == 1
+    assert  changed_values[0][0] == "colleagues" # Backingstore should detect manager property changed
+    colleagues = mock_user.backing_store.get("colleagues")
+    assert colleagues[0].id == "2f8c9b8c-8ea0-46b2-9c6d-660c9b7bf6af"
+    
+def test_backing_store_embedded_in_model_by_updating_nested_backed_model_collection_property_returns_changed_nested_properties():
+    mock_user = MockEntity(
+        id="84c747c1-d2c0-410d-ba50-fc23e0b4abbe",
+        colleagues=[MockEntity(id="2f8c9b8c-8ea0-46b2-9c6d-660c9b7bf6af")]
+    )
+    mock_user.backing_store.is_initialization_completed = mock_user.colleagues[0].backing_store.is_initialization_completed = True
+    mock_user.colleagues[0].business_phones = ["+1 234 567 891"]
+    mock_user.backing_store.return_only_changed_values = True
+    mock_user.colleagues[0].backing_store.return_only_changed_values = True
+    changed_values = mock_user.backing_store.enumerate_()
+    assert len(changed_values) == 1
+    assert  changed_values[0][0] == "colleagues" # Backingstore should detect manager property changed
+    nested_changed_values = mock_user.colleagues[0].backing_store.enumerate_()
+    assert len(nested_changed_values) == 1
+    assert nested_changed_values[0][0] == "business_phones"
+    
+def test_backing_store_embedded_in_model_by_updating_nested_backed_model_collection_property_with_extra_value_returns_changed_nested_properties():
+    mock_user = MockEntity(
+        id="84c747c1-d2c0-410d-ba50-fc23e0b4abbe",
+        colleagues=[MockEntity(
+            id="2f8c9b8c-8ea0-46b2-9c6d-660c9b7bf6af",
+            business_phones=["+1 234 567 891"]
+            )]
+    )
+    mock_user.backing_store.is_initialization_completed = mock_user.colleagues[0].backing_store.is_initialization_completed = True
+    mock_user.colleagues[0].business_phones.append("+9 876 543 219")
+    mock_user.backing_store.return_only_changed_values = True
+    mock_user.colleagues[0].backing_store.return_only_changed_values = True
+    changed_values = mock_user.backing_store.enumerate_()
+    assert len(changed_values) == 1
+    assert  changed_values[0][0] == "colleagues" # Backingstore should detect manager property changed
+    nested_changed_values = mock_user.colleagues[0].backing_store.enumerate_()
+    assert len(nested_changed_values) == 1
+    assert nested_changed_values[0][0] == "business_phones"
+    assert nested_changed_values[0][1] == (["+1 234 567 891", "+9 876 543 219"], 2)
+    
+def test_backing_store_embedded_in_model_by_updating_nested_backed_model_collection_property_with_extra_backed_model_returns_changed_nested_properties():
+    mock_user = MockEntity(
+        id="84c747c1-d2c0-410d-ba50-fc23e0b4abbe",
+        colleagues=[MockEntity(
+            id="2f8c9b8c-8ea0-46b2-9c6d-660c9b7bf6af",
+            business_phones=["+1 234 567 891"]
+            )]
+    )
+    mock_user.backing_store.is_initialization_completed = mock_user.colleagues[0].backing_store.is_initialization_completed = True
+    mock_user.colleagues.append(MockEntity(id="2fe22fe5-1132-42cf-90f9-1dc17e325a74"))
+    mock_user.backing_store.return_only_changed_values = True
+    mock_user.colleagues[0].backing_store.return_only_changed_values = True
+    changed_values = mock_user.backing_store.enumerate_()
+    assert len(changed_values) == 1
+    assert  changed_values[0][0] == "colleagues" # Backingstore should detect manager property changed
+    colleagues = changed_values[0][1]
+    assert len(colleagues) == 2
+    assert colleagues[0][0].id == "2f8c9b8c-8ea0-46b2-9c6d-660c9b7bf6af"
+    assert colleagues[0][1].id == "2fe22fe5-1132-42cf-90f9-1dc17e325a74"
+    nested_changed_values = mock_user.colleagues[0].backing_store.enumerate_()
+    assert len(nested_changed_values) == 0
+        
+        
+    
+    
+    
+    
+
+        
