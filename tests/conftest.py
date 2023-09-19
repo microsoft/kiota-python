067b9c1c7336732980aabb6e9ecfc3831664d2bc
samwelkanda <samwelkanda@gmail.com>
2023-09-19 08:52:10 +0300

    Add tests for base request builder

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,39 +5,48 @@
 # ------------------------------------------------------------------------------
 
 from __future__ import annotations
+
 from dataclasses import dataclass, field
-from typing import Callable, List, Any, Tuple, Dict, Optional
+from typing import Any, Callable, Dict, List, Optional, Tuple
+
 import pytest
 
 from kiota_abstractions.authentication.access_token_provider import AccessTokenProvider
-from kiota_abstractions.authentication.allowed_hosts_validator import (
-    AllowedHostsValidator,
-)
+from kiota_abstractions.authentication.allowed_hosts_validator import AllowedHostsValidator
+from kiota_abstractions.request_adapter import RequestAdapter
 from kiota_abstractions.request_information import RequestInformation
-from kiota_abstractions.serialization import AdditionalDataHolder, Parsable, ParseNode, SerializationWriter
+from kiota_abstractions.serialization import (
+    AdditionalDataHolder,
+    Parsable,
+    ParseNode,
+    SerializationWriter,
+)
 from kiota_abstractions.store import BackedModel, BackingStore, BackingStoreFactorySingleton
 
 
 class MockAccessTokenProvider(AccessTokenProvider):
+
     def __init__(self):
         self.token = None
 
     async def get_authorization_token(
-        self,
-        url: str,
-        additional_authentication_context: Dict[str, Any] = {}
+        self, url: str, additional_authentication_context: Dict[str, Any] = {}
     ) -> str:
         return "SomeToken"
 
     def get_allowed_hosts_validator(self) -> AllowedHostsValidator:
         return AllowedHostsValidator(["example.com"])
 
+
 @dataclass
 class MockEntity(Parsable, AdditionalDataHolder, BackedModel):
     # Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.
     additional_data: Dict[str, Any] = field(default_factory=dict)
     # Stores model information.
-    backing_store : BackingStore = field(default_factory=BackingStoreFactorySingleton(backing_store_factory=None).backing_store_factory.create_backing_store)
+    backing_store: BackingStore = field(
+        default_factory=BackingStoreFactorySingleton(backing_store_factory=None
+                                                     ).backing_store_factory.create_backing_store
+    )
     # The id property
     id: Optional[str] = None
     # The OdataType property
@@ -48,7 +57,7 @@ class MockEntity(Parsable, AdditionalDataHolder, BackedModel):
     manager: Optional[MockEntity] = None
     # The user or contact that is this user& works with.
     colleagues: Optional[List[MockEntity]] = None
-    
+
     @staticmethod
     def create_from_discriminator_value(parse_node: Optional[ParseNode] = None):
         """
@@ -60,22 +69,27 @@ class MockEntity(Parsable, AdditionalDataHolder, BackedModel):
         if not parse_node:
             raise TypeError("parse_node cannot be null.")
         return MockEntity()
-    
-    def get_field_deserializers(self,) -> Dict[str, Callable[[ParseNode], None]]:
+
+    def get_field_deserializers(self, ) -> Dict[str, Callable[[ParseNode], None]]:
         """
         The deserialization information for the current model
         Returns: Dict[str, Callable[[ParseNode], None]]
         """
         fields: Dict[str, Callable[[Any], None]] = {
-            "@odata.type": lambda n : setattr(self, 'odata_type', n.get_str_value()),
-            "id": lambda n : setattr(self, 'id', n.get_str_value()),
-            "businessPhones": lambda n : setattr(self, 'business_phones', n.get_collection_of_primitive_values(str)),
-            "manager": lambda n : setattr(self, 'manager', n.get_object_value(MockEntity)),
-            "colleagues": lambda n : setattr(self, 'colleagues', n.get_collection_of_object_values(MockEntity)),
+            "@odata.type":
+            lambda n: setattr(self, 'odata_type', n.get_str_value()),
+            "id":
+            lambda n: setattr(self, 'id', n.get_str_value()),
+            "businessPhones":
+            lambda n: setattr(self, 'business_phones', n.get_collection_of_primitive_values(str)),
+            "manager":
+            lambda n: setattr(self, 'manager', n.get_object_value(MockEntity)),
+            "colleagues":
+            lambda n: setattr(self, 'colleagues', n.get_collection_of_object_values(MockEntity)),
         }
         return fields
-    
-    def serialize(self,writer: SerializationWriter) -> None:
+
+    def serialize(self, writer: SerializationWriter) -> None:
         """
         Serializes information the current object
         Args:
@@ -89,7 +103,8 @@ class MockEntity(Parsable, AdditionalDataHolder, BackedModel):
         writer.write_collection_of_primitive_values("businessPhones", self.business_phones)
         writer.write_object_value("manager", self.manager)
         writer.write_collection_of_object_values("colleagues", self.colleagues)
-    
+
+
 @pytest.fixture
 def mock_user():
     user = MockEntity()
@@ -98,6 +113,7 @@ def mock_user():
     user.business_phones = ["+1 732 555 0102"]
     user.backing_store.is_initialization_completed = True
     return user
+
 
 @pytest.fixture
 def mock_request_information():
@@ -109,3 +125,9 @@ def mock_request_information():
 @pytest.fixture
 def mock_access_token_provider():
     return MockAccessTokenProvider()
+
+
+@pytest.fixture
+def mock_request_adapter(mocker):
+    mocker.patch.multiple(RequestAdapter, __abstractmethods__=set())
+    return RequestAdapter()
