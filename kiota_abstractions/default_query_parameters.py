77be2a2c334156eb2ffc560a3ee9756eb60a6168
samwelkanda <samwelkanda@gmail.com>
2024-02-05 15:36:05 +0300

    Add generic query parameters

--- kiota_abstractions/default_query_parameters.py
+++ kiota_abstractions/default_query_parameters.py
@@ -4,6 +4,7 @@
 # See License in the project root for license information.
 # ------------------------------------
 from dataclasses import dataclass
+from typing import List, Optional
 from warnings import warn
 
 
@@ -12,7 +13,26 @@ class QueryParameters:
     """
     Default placeholder class for query parameters.
     """
-    pass
+    # Include count of items
+    count: Optional[bool] = None
+
+    # Expand related entities
+    expand: Optional[List[str]] = None
+
+    # Filter items by property values
+    filter: Optional[str] = None
+
+    # Order items by property values
+    orderby: Optional[List[str]] = None
+
+    # Select properties to be returned
+    select: Optional[List[str]] = None
+
+    # Skip the first n items
+    skip: Optional[int] = None
+
+    # Show only the first n items
+    top: Optional[int] = None
 
 
 @dataclass
