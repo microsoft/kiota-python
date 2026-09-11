b06ac4d5edc0c12c9b29a46b63ea4539a20ca8b7
RKS <rajesh.sharma@owasp.org>
2026-09-11 12:04:04 -0400

    fix(serialization-json): serialize nested arrays recursively

--- packages/serialization/json/kiota_serialization_json/json_serialization_writer.py
+++ packages/serialization/json/kiota_serialization_json/json_serialization_writer.py
@@ -488,10 +488,7 @@ class JsonSerializationWriter(SerializationWriter):
             elif all(isinstance(x, dict) for x in value):
                 self.__write_collection_of_dict_values(key, value)
             else:
-                raise TypeError(
-                    f"Encountered an unknown collection type during serialization {type(value)}\
-                    with key {key}"
-                )
+                self.write_collection_of_primitive_values(key, value)
         elif isinstance(value, dict):
             self.__write_dict_value(key, value)
         else:
